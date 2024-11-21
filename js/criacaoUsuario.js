e6b66b88a0687819fe9eea236fd0d1b260170c1b
airtonbjunior <airtonbjunior@gmail.com>
2024-11-21 08:58:42 -0300

    aula 21/11

--- js/criacaoUsuario.js
+++ js/criacaoUsuario.js
@@ -1,6 +1,21 @@
 const btnCriaUsuario = document.getElementById("btn-criar-usuario");
 
 btnCriaUsuario.addEventListener("click", async () => {
-    const resposta = await fetch("http://localhost:3000/usuarios");
-    console.log(resposta);
+    
+    const reposta = await fetch('http://localhost:3000/usuario', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            nome: document.getElementById("nome").value,
+            email: document.getElementById("email").value,
+            login: document.getElementById("login").value,
+            senha: document.getElementById("senha").value
+        })
+    })
+    .then(() => {
+        console.log("Usuário criado com sucesso!");
+    })
+    .catch(error => {
+        console.log(error);
+    });
 });
